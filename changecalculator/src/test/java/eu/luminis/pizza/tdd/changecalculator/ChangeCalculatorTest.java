5ae34eab703e2ced948dbdfc3c7db498bcce2d46
Bjorn Lammers <bjorn.lammers@luminis.eu>
2018-01-08 19:42:04 +0100

    Bug #1: added tests to prove this bug, and ones like it

--- changecalculator/src/test/java/eu/luminis/pizza/tdd/changecalculator/ChangeCalculatorTest.java
+++ changecalculator/src/test/java/eu/luminis/pizza/tdd/changecalculator/ChangeCalculatorTest.java
@@ -24,4 +24,28 @@ public class ChangeCalculatorTest {
         final Map<Denomination, Integer> change = ChangeCalculator.calculateChange(377, 377);
         assertTrue(change.isEmpty());
     }
+
+    @Test
+    public void calculateChange_WrongDenominations() {
+        final Map<Denomination, Integer> change = ChangeCalculator.calculateChange(87, 100);
+        assertEquals(3, change.size());
+        assertThat(change, IsMapContaining.hasEntry(Denomination.TEN, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.TWO, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.ONE, 1));
+    }
+
+    @Test
+    public void calculateChange_AllDenominations() {
+        final Map<Denomination, Integer> change = ChangeCalculator.calculateChange(222, 1000);
+        assertEquals(9, change.size());
+        assertThat(change, IsMapContaining.hasEntry(Denomination.FIVE_HUNDRED, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.TWO_HUNDRED, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.HUNDRED, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.FIFTY, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.TWENTY, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.TEN, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.FIVE, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.TWO, 1));
+        assertThat(change, IsMapContaining.hasEntry(Denomination.ONE, 1));
+    }
 }
